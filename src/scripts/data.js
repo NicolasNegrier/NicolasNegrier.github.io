86756a8cd7e66a44285739c79cb8f82210851a21
NicolasNegrier <nicolas.negrier@gmail.com>
2023-03-21 18:52:46 +0100

    new version biodanza

---
+++ src/scripts/data.js
@@ -0,0 +1,20 @@
+const data =
+{
+    events: [
+        {
+            "title": "Le cercle des archetypes feminins",
+            "description": "Notre nature de femme est composée de multiples couleurs et facettes !Femmes, nous vous invitons à éveiller et danser toutes les déesses qui nous habitent, nous font vibrer, agir, sentir…Ce stage pour les connaître, les reconnaître en nous, les honorer et s’en faire des alliées sur nos chemins de vie. Dans la danse permettre leur émergence du plus profond de nos inconscients de femmes pour augmenter nos possibilités, nos ressources et notre créativité existentielle. ",
+            "date": "24,25,26 février 2023"
+        },
+        {
+            "title": "Instructivement connectés à la vie",
+            "description": "Ce stage est une invitation à retrouver la fonction archaïque de connexion à la vie si primordiale pour nous-même et bien au-delà pour notre rapport au monde et au vivant !",
+            "date": "du 19 au 23 juillet 2023"
+        },
+        {
+            "title": "Super titre",
+            "description": "Le contenu de mon evenement",
+            "date": ""
+        },
+    ],
+};
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -13,130 +13,43 @@ const app = {
         burgerToggler.addEventListener('click', toggleNav);
         navLinksContainer.addEventListener('click', toggleNav);
     },
-    
-    // Animation Text header
-    headerAnimate: () => {
-        const h1HeaderElm = document.querySelector('header h1');
 
-        const writeH1 = new Typewriter(h1HeaderElm, {
-            loop: false,
-        });
+    // Chargement du contenu de data.js
+    dataLoad: () => {
 
-        writeH1
-            .typeString('Developpeur Web')
-            .start();
+        for (i=0 ; i < data.events.length ; i++ ) {
 
-
-        const h2HeaderElm = document.querySelector('header h2');
-
-        const writeH2 = new Typewriter(h2HeaderElm, {
-            loop: true,
-            delay: 175,
-        });
-
-        writeH2
-            // .typeString('FrontEnd <span style="color: #FF4500" class="strong">HTML</span>')
-            // .pauseFor(2000)
-            // .deleteChars(4)
-            // .typeString('<span style="color: #01438E" class="strong"> CSS</span>')
-            // .pauseFor(2000)
-            // .deleteChars(3)
-            .typeString('FrontEnd <span style="color: #F0DB4F" class="strong"> JavaScript</span>')
-            .pauseFor(2000)
-            .deleteChars(10)
-            .typeString('<span style="color: #61dafb" class="strong"> React</span>')
-            .pauseFor(2000)
-            .deleteAll()
-            .typeString('BackEnd <span style="color: #83CD29" class="strong"> Node</span>')
-            .pauseFor(2000)
-            .deleteChars(4)
-            .typeString('<span class="strong"> Express</span>')
-            .pauseFor(2000)
-            .deleteChars(7)
-            .typeString('<span style="color: #2379BD" class="strong"> Sequelize</span>')
-            .pauseFor(2000)
-            .deleteAll()
-            .typeString('FullStack JavaScript')
-            .pauseFor(2000)
-            .deleteAll()
-            .start();
-    },
-
-    // Apparition des elements au scroll
-    
-    scrollListner: () => {
-        const ratio = 0.30;
-        const observer = new IntersectionObserver(app.handleScroll, {threshold: ratio});
-
-        const hardSkillElms = document.querySelectorAll('.hard-skill__cat');
-        hardSkillElms.forEach(hardSkillElm => {
-            observer.observe(hardSkillElm);
-        })
-
-        const softSkillElms = document.querySelectorAll('.soft-skill__item');
-        softSkillElms.forEach(softSkillElm => {
-            observer.observe(softSkillElm);
-        })
-        
-    },
-
-    handleScroll: (entries) => {
-        entries.forEach(entry => {
-
-            const targetElm = entry.target;
-            const softSkillFirstChildElm = entry.target.firstElementChild;
-            const softSkillLastChildElm = entry.target.lastElementChild;
-
-            if (entry.isIntersecting) {
-                
-                if (targetElm.classList[0] === "hard-skill__cat") {
-                    targetElm.classList.add('translateY');
-
-                }else if (targetElm.classList[0] === "soft-skill__item") {
-    
-                    targetElm.classList.add('translateX-droite');
-                    softSkillFirstChildElm.classList.add('translateX-gauche');
-                    softSkillLastChildElm.classList.add('translateX-gauche');
-                }
-            }else {
-                if (targetElm.classList[0] === "hard-skill__cat") {
-
-                    targetElm.classList.remove('translateY');
-                    
-                }else if (targetElm.classList[0] === "soft-skill__item") {
-
-                    targetElm.classList.remove('translateX-droite');
-                    softSkillFirstChildElm.classList.remove('translateX-gauche');
-                    softSkillLastChildElm.classList.remove('translateX-gauche');
-                }
-            }
-        });
-        
-    },
-
-    // Changement de la class colomn et row suivant la taille de l'ecran
-    windowSeize: () => {
-        
-        const seize = window.innerWidth;
-        const hardSkillElm = document.querySelector('#hard-skill');
-        const softSkillElm = document.querySelector('#soft-skill');
-
-        if(seize > 1000) {
-            hardSkillElm.classList.remove('column');
-            hardSkillElm.classList.add('row');
-            softSkillElm.classList.add('row');
-        }else {
-            hardSkillElm.classList.remove('row');
-            hardSkillElm.classList.add('column');
-            softSkillElm.classList.remove('row');
+            number = i + 1;
+            app.cardCreate(number);
+            // Creation d'un H3 pour le titre dans la div title du header
+            let title = document.createElement('h4');
+            title.innerText = data.events[i].title;
+            // Creation d'un descriptif
+            let desc = document.createElement('p');
+            desc.innerText = data.events[i].description;
+            // Creation d'une date
+            let date = document.createElement('p');
+            date.innerText = data.events[i].date;
+            // Récupération du parent et ajout de notre titre au parent
+            let parentTitle = document.getElementById('card'+ number );
+            parentTitle.appendChild(title);
+            parentTitle.appendChild(desc);
+            parentTitle.appendChild(date);
         }
     },
 
+    // Creation d'une card
+    cardCreate: (number) => {
+        let card = document.createElement('div');
+        card.classList.add('card');
+        card.id = 'card' + number;
+        let cardParent = document.getElementById('section4__cards');
+        cardParent.appendChild(card);
+    },
+
     init: () => {
-        // app.headerAnimate();
-        // app.scrollListner();
-        // app.windowSeize();
         app.toggleMenu();
+        app.dataLoad();
     },
 };
 
